5cf796d0ec88ab24673cf3e567cdb97be74cadaa
Chris Keller <ctk8501@gmail.com>
2026-07-26 16:07:02 -0400

    wip

--- src/SchemaAudit.php
+++ src/SchemaAudit.php
@@ -28,7 +28,10 @@ final readonly class SchemaAudit implements Arrayable, Jsonable, JsonSerializabl
      */
     public function withFindings(array $findings): self
     {
-        return new self([...$this->findings, ...$findings]);
+        return new self([
+            ...$this->findings,
+            ...$findings,
+        ]);
     }
 
     public function count(): int
