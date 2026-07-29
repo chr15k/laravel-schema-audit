cb799883037b1dd56a5423783d05ad90d1904d33
Chris Keller <ctk8501@gmail.com>
2026-07-29 08:27:54 -0400

    pest v5

--- rector.php
+++ rector.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
+use Pest\Rector\Set\PestSetList;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
+    ])
+    ->withSets([
+        PestSetList::CODING_STYLE,
     ])
     ->withPreparedSets(
         deadCode: true,
